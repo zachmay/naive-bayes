04dc77f8624e3fe95e20b3c3fa10cbf7908f5dd1
Zachary May <zach@sector42.net>
2014-02-17 18:13:21 -0500

    Added support for the binary counting case

--- nb.py
+++ nb.py
@@ -27,10 +27,7 @@ class Classifier:
         if documents is not None:
             self.train(documents)
 
-    def train(self, documents):
-        """
-        Train the classifier
-        """
+    def _initialize(self):
         self.class_token_counts = defaultdict(lambda: defaultdict(int)) # Count of occurrences of a given token in a given class.
         self.class_global_counts = defaultdict(int)                     # Count of all tokens occurring in a given class.
         self.class_counts = defaultdict(int)                            # Count of documents in a given class.
@@ -38,6 +35,13 @@ class Classifier:
 
         self.priors = defaultdict(float)                                         # Parameter table: prior probability for a given class
         self.conditional_probabilities = defaultdict(lambda: defaultdict(float)) # Parameter table: for tag c and token t, P(t|c)
+
+    def train(self, documents):
+        """
+        Train the classifier
+        """
+
+        self._initialize()
 
         doc_number = 0
 
@@ -48,24 +52,18 @@ class Classifier:
                     self._count_token(doc_number, tag, token)
             doc_number += 1
 
+        self.document_count = doc_number
         self.classes = self.class_counts.keys()
 
-        print "Vocabulary", self.vocabulary
-        print "Class token counts", self.class_token_counts
-        print "Class global counts", self.class_global_counts
-        print "Document count", doc_number
-        print "Class counts", self.class_counts
-
         for c in self.classes:
-            self.priors[c] = self.class_counts[c] / float(doc_number)
-            self.conditional_probabilities[c] = {}
+            self.priors[c] = self.class_counts[c] / float(self.document_count)
+            #self.conditional_probabilities[c] = {}
             for t in self.vocabulary:
                 self.conditional_probabilities[c][t] = self._compute_conditional_probability(c, t)
 
         self.is_trained = True
 
     def _compute_conditional_probability(self, tag, token):
-        print "_compute_conditional_probability", tag, token
         numer = float(self.class_token_counts[tag][token] + 1) 
         denom = float(self.class_global_counts[tag] + len(self.vocabulary))
         return numer / denom
@@ -92,6 +90,40 @@ class Classifier:
 
         self.vocabulary.add(token)
 
+    def parameter_dump(self):
+        """
+        Print a (very verbose) dump of the counts and 
+        computed parameters learned by the training
+        documents.
+        """
+        if not self.is_trained:
+            raise Error('Classifier not trained!')
+
+        print "Document count:  ", self.document_count
+        print "Vocabulary count:", len(self.vocabulary)
+
+        print "Class counts:"
+        for c in self.class_counts:
+            print "  ", c, "=", self.class_counts[c]
+
+        print "Class global counts:"
+        for c in self.class_global_counts:
+            print "  ", c, "=", self.class_global_counts[c]
+
+        print "Class token counts:"
+        for c in self.class_token_counts:
+            for t in self.class_token_counts[c]:
+                print "  ", c, t, "=", self.class_token_counts[c][t]
+
+        print "Priors:"
+        for c in self.classes:
+            print "  ", c, "-", self.priors[c]
+
+        print "Conditional Probabilities:"
+        for c in self.classes:
+            for t in self.vocabulary:
+                print "  ", "P(" + str(t) + "|" + str(c) + ") =", self.conditional_probabilities[c][t]
+
     def classify(self, document):
         """
         Classify the given document (a stream of tokens)
@@ -108,6 +140,10 @@ class BinaryClassifier(Classifier):
     def __init__(self, stop_words, documents=None):
         Classifier.__init__(self, stop_words, documents)
 
+    def _initialize(self):
+        self.document_tokens_seen = defaultdict(set)
+        Classifier._initialize(self)
+
     def _count_token(self, doc_number, tag, token):
         """
         Update the count in the vocabulary for the given token.
@@ -117,17 +153,27 @@ class BinaryClassifier(Classifier):
         """
         self.vocabulary.add(token)
 
-        # Update count for this token for the given class tag.
-        self.class_token_counts[tag][token] += 1
+        if token not in self.document_tokens_seen[doc_number]:
+            self.document_tokens_seen[doc_number].add(token)
 
-        # Update count of all tokens for this the given class tag.
-        self.class_global_counts[tag] += 1
+            # Update count for this token for the given class tag.
+            self.class_token_counts[tag][token] += 1
+
+            # Count this token
+            self.class_global_counts[tag] += 1
 
 class Tester:
+    """
+    Harness for testing a (trained) classifier.
+    """
     def __init__(self, classifier, documents=None):
         self.classifier = classifier
         if documents is not None:
-            sel
+            self.test(documents)
+
+    def test(self, documents):
+        pass
+
 
 
 
